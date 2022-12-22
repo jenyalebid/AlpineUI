a64bda3d4908db8fecbf6896197d4f175be4e15b
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2022-12-22 15:54:44 -0800

    Added new double to time extension.

--- Sources/AlpineUI/Extensions/Double.swift
+++ Sources/AlpineUI/Extensions/Double.swift
@@ -32,4 +32,12 @@ extension Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
     }
+    
+    public func secondsToTime(units: NSCalendar.Unit = [.hour, .minute, .second], style: DateComponentsFormatter.UnitsStyle = .full) -> String {
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = units
+        formatter.unitsStyle = .full
+
+        return formatter.string(from: TimeInterval(self)) ?? "---"
+    }
 }
