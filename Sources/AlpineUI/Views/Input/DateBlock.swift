4fe266e4957cbb9a6a1e4992fed425d5e66c17b7
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-01-19 07:19:57 -0800

    Added new date block.

---
+++ Sources/AlpineUI/Views/Input/DateBlock.swift
@@ -0,0 +1,61 @@
+//
+//  DateBlock.swift
+//  
+//
+//  Created by Jenya Lebid on 1/18/23.
+//
+
+import SwiftUI
+
+public struct DateBlock: View {
+    
+    @Binding var date: Date
+    @State var showPicker = false
+
+    var title: String
+    var components: DatePicker<Label>.Components = .date
+    var actionOnDismiss: (() -> ())?
+    
+    public init(title: String, date: Binding<Date>, components: DatePickerComponents = .date, actionOnDismiss: (() -> ())? = nil) {
+        self.title = title
+        self._date = date
+        self.components = components
+        self.actionOnDismiss = actionOnDismiss
+    }
+    
+    public var body: some View {
+        VStack(alignment: .leading, spacing: 2) {
+            if title != "" {
+                Text("\(title):")
+                    .font(.footnote)
+            }
+            Text(date.toString(format: "MMM dd, yyyy"))
+                .padding(8)
+                .foregroundColor(Color(uiColor: .label))
+                .background(Color(uiColor: .systemGray6))
+                .cornerRadius(5)
+                .popover(isPresented: $showPicker) {
+                    DatePicker("", selection: $date, displayedComponents: components)
+                        .datePickerStyle(.graphical)
+                        .frame(width: 320, height: 320)
+                        .onDisappear {
+                            if let actionOnDismiss {
+                                actionOnDismiss()
+                            }
+                        }
+                }
+        }
+        .onTapGesture {
+            if !showPicker {
+                showPicker = true
+            }
+        }
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+        
+    static var previews: some View {
+        DateBlock(title: "Date", date: .constant(Date()))
+    }
+}
