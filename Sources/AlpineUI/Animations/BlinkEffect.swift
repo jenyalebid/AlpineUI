77faea34a9567bcc1459463867691ad8aabb8073
mkv <kons@devake.com>
2023-03-22 17:02:02 +0600

    min&max values

--- Sources/AlpineUI/Animations/BlinkEffect.swift
+++ Sources/AlpineUI/Animations/BlinkEffect.swift
@@ -10,12 +10,23 @@ import SwiftUI
 public struct BlinkEffect: ViewModifier {
     
     @State private var animate = false
+    var minValue = 0.0
+    var maxValue = 1.0
     
-    public init() {}
+    public init(minimum: Double = 0, maximum: Double = 1) {
+        minValue = minimum
+        maxValue = maximum
+        if minValue < 0 || minValue > 1 {
+            minValue = 0
+        }
+        if maxValue < 0 || maxValue > 1 {
+            maxValue = 1
+        }
+    }
     
     public func body(content: Content) -> some View {
         content
-            .opacity(animate ? 1 : 0)
+            .opacity(animate ? maxValue : minValue)
             .animation(animate ? Animation.easeInOut(duration: 1.5).repeatForever(autoreverses: true) : .default, value: animate)
             .onAppear {
                 animate = true
@@ -26,7 +37,7 @@ public struct BlinkEffect: ViewModifier {
 
 extension View {
     
-    public func blinkEffect() -> some View {
-        return modifier(BlinkEffect())
+    public func blinkEffect(minimum: Double = 0, maximum: Double = 1) -> some View {
+        return modifier(BlinkEffect(minimum: minimum, maximum: maximum))
     }
 }
