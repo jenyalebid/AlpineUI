741c1b48ab0cf34afa00fe20e65db2683b73041f
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-06-15 15:56:02 -0700

    Create ShakeEffect.swift

---
+++ Sources/AlpineUI/Animations/ShakeEffect.swift
@@ -0,0 +1,50 @@
+//
+//  ShakeEffect.swift
+//  AlpineUI
+//
+//  Created by Jenya Lebid on 6/15/23.
+//
+
+import SwiftUI
+
+struct ShakeEffect: ViewModifier {
+    
+    @State private var shakes: CGFloat = 0
+    
+    func body(content: Content) -> some View {
+        content
+            .modifier(Shake(animatableData: shakes))
+            .simultaneousGesture(
+                TapGesture()
+                    .onEnded {
+                        withAnimation(.linear(duration: 0.25)) {
+                            shakes += 1
+                        }
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+                            withAnimation {
+                                shakes = 0
+                            }
+                        }
+                        
+                    }
+            )
+    }
+}
+
+public extension View {
+    
+    var shakeOnTap: some View {
+        modifier(ShakeEffect())
+    }
+}
+
+struct Shake: GeometryEffect {
+    var amount: CGFloat = 5
+    var shakesPerUnit = 2
+    var animatableData: CGFloat
+
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX: amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)), y: 0))
+    }
+}
