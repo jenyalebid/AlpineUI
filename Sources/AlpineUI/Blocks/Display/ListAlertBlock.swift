7f3e76cfb23e8ab351e3a3700e8bbb1e26c21ebf
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-08-22 15:59:03 -0700

    Update ListAlertBlock.swift

--- Sources/AlpineUI/Blocks/Display/ListAlertBlock.swift
+++ Sources/AlpineUI/Blocks/Display/ListAlertBlock.swift
@@ -13,36 +13,42 @@ public struct ListAlertBlock: View {
     var notExported: Bool
     var deleted: Bool
     
-    public init(missingRequirements: Bool, notExported: Bool, deleted: Bool = false) {
+    var showExported: Bool
+    
+    public init(missingRequirements: Bool, notExported: Bool, deleted: Bool = false, showExported: Bool = false) {
         self.missingRequirements = missingRequirements
         self.notExported = notExported
         self.deleted = deleted
+        self.showExported = showExported
     }
     
     
     public var body: some View {
         VStack(alignment: .leading) {
-            if missingRequirements || notExported || deleted {
+            if missingRequirements || notExported || deleted || showExported {
                 Divider()
                     .frame(width: 20)
             }
             if missingRequirements || deleted {
                 Text(deleted ? "Deleted" : "Missing Required Fields")
                     .foregroundColor(Color.red)
-                    .font(.caption2)
             }
             if notExported {
                 Text("Not Exported")
                     .foregroundColor(Color("NotExported"))
-                    .font(.caption2)
+            }
+            else if showExported {
+                Text("Exported")
+                    .foregroundColor(Color.green)
             }
         }
+        .font(.caption2)
         .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
 struct ListAlertBlock_Previews: PreviewProvider {
     static var previews: some View {
-        ListAlertBlock(missingRequirements: true, notExported: true)
+        ListAlertBlock(missingRequirements: true, notExported: false, showExported: true)
     }
 }
