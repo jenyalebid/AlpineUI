769c2846b4927964cb2bb37fe8b197987932cf27
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-10-24 15:53:07 -0700

    Create DismissButton.swift

---
+++ Sources/AlpineUI/DismissButton.swift
@@ -0,0 +1,49 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Jenya Lebid on 10/24/23.
+//
+
+import SwiftUI
+
+@available(iOS 17, *)
+public struct DismissButton: View {
+
+    var environmentDismiss: Bool
+    var action: (() -> Void)?
+    
+    @Environment(\.dismiss) var dismiss
+    
+    public init(environmentDismiss: Bool = true, action: (() -> Void)? = nil) {
+        self.environmentDismiss = environmentDismiss
+        self.action = action
+    }
+    
+    public var body: some View {
+        Button(role: .cancel) {
+            if environmentDismiss {
+                dismiss()
+            }
+            if let action {
+                withAnimation {
+                    action()
+                }
+            }
+        } label: {
+            Image(systemName: "xmark")
+                .font(.headline)
+        }
+        .buttonStyle(.bordered)
+        .buttonBorderShape(.circle)
+        .tint(.secondary)
+    }
+}
+
+#Preview {
+    if #available(iOS 17, *) {
+        DismissButton()
+    } else {
+        EmptyView()
+    }
+}
