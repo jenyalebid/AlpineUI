a53a5aa35e7aa8ef4700540c3aac46947900a870
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2023-12-04 16:07:30 -0800

    Update ListTextFieldBlock.swift

--- Sources/AlpineUI/Blocks/Default List/ListTextFieldBlock.swift
+++ Sources/AlpineUI/Blocks/Default List/ListTextFieldBlock.swift
@@ -54,6 +54,28 @@ public struct ListTextFieldBlock: View {
     }
 }
 
+public struct ListIconTextFieldBlock: View {
+    
+    var systemImage: String
+    var title: String
+    
+    public init(systemImage: String, title: String) {
+        self.systemImage = systemImage
+        self.title = title
+    }
+    
+    public var body: some View {
+        HStack {
+            Image(systemName: systemImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 20, height: 20, alignment: .center)
+                .padding(.trailing, 4)
+            Text(title)
+        }
+    }
+}
+
 struct ListTextFieldBlock_Previews: PreviewProvider {
     static var previews: some View {
         List {
