15b5df42cdc7ac265e27e917c76a2ed1f6e98f8d
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2024-02-05 15:58:45 -0800

    Update WebView.swift

--- Sources/AlpineUI/Custom/WebView.swift
+++ Sources/AlpineUI/Custom/WebView.swift
@@ -8,20 +8,80 @@
 import SwiftUI
 import WebKit
 
-public struct WebView: UIViewRepresentable {
+public struct WebView: View {
     
-    let url: URL
+    @Binding var url: URL
+    @State private var isLoading = false
     
-    public init(url: URL) {
-        self.url = url
+    public init(url: Binding<URL>) {
+        self._url = url
     }
     
-    public func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
+    public var body: some View {
+        ZStack {
+            WebViewRepresentable(url: $url, isLoading: $isLoading)
+            if isLoading {
+                Rectangle()
+                    .fill(Color.black.opacity(0.5))
+                ProgressView()
+                    .scaleEffect(1.5)
+            }
+        }
+        .navigationBarTitleDisplayMode(.inline)
     }
+}
 
-    public func updateUIView(_ webView: WKWebView, context: Context) {
-        let request = URLRequest(url: url)
+fileprivate struct WebViewRepresentable: UIViewRepresentable {
+    
+    @Binding var isLoading: Bool
+    @Binding var url: URL
+    
+    private var initialURL: URL
+    
+    init(url: Binding<URL>, isLoading: Binding<Bool>) {
+        self._url = url
+        self._isLoading = isLoading
+        
+        self.initialURL = url.wrappedValue
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        let request = URLRequest(url: initialURL)
         webView.load(request)
+        
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebViewRepresentable
+        
+        init(_ webView: WebViewRepresentable) {
+            self.parent = webView
+        }
+        
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+            parent.isLoading = true
+        }
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            parent.isLoading = false
+            if webView.url != parent.initialURL {
+                parent.url = webView.url ?? parent.initialURL
+            }
+        }
+        
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            parent.isLoading = false
+        }
     }
 }
