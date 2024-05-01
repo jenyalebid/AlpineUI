e1055f7a5696832fdca6728d2a0f0020e818a12e
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2024-05-01 16:00:55 -0700

    Update Date.swift

--- Sources/AlpineUI/Extensions/Date.swift
+++ Sources/AlpineUI/Extensions/Date.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension Date {
+public extension Date {
     
     public func toString(format: String = "yyyy-MM-dd") -> String {
         let formatter = DateFormatter()
@@ -23,5 +23,14 @@ extension Date {
         let strSec = String(format: "%02d", difference.second ?? 00)
         
         return "\(strMin):\(strSec)"
+    }
+    
+    
+    func hoursAndMinutes(to date: Date) -> (hours: Int, minutes: Int) {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.hour, .minute], from: self, to: date)
+        let hours = components.hour ?? 0
+        let totalMinutes = components.minute ?? 0
+        return (hours, totalMinutes)
     }
 }
