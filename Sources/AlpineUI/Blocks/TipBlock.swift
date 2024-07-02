a99976d108b0a26c208294d795c9aa0f6b677167
Murygin Vladislav <muriginvlad@gmail.com>
2024-07-02 17:09:58 -0500

    update TipBlock

--- Sources/AlpineUI/Blocks/TipBlock.swift
+++ Sources/AlpineUI/Blocks/TipBlock.swift
@@ -10,18 +10,21 @@ import TipKit
 
 public struct TipBlock<T: Tip>: View {
     
-    public init(_ tip: T) {
+    private var tip: T
+    private var actionHandler: ((Tips.Action) -> Void)?
+
+    public init(_ tip: T, actionHandler: ((Tips.Action) -> Void)? = nil) {
         self.tip = tip
+        self.actionHandler = actionHandler
     }
-    
-    var tip: T
-    
+
     public var body: some View {
-        TipView(tip)
-            .tipBackground(Color.accentColor.opacity(0.10))
-            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-            .listRowBackground(EmptyView())
-            .listRowSeparator(.hidden)
+        TipView(tip) { action in
+            actionHandler?(action)
+        }
+        .tipBackground(Color.accentColor.opacity(0.10))
+        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+        .listRowBackground(EmptyView())
+        .listRowSeparator(.hidden)
     }
 }
-
